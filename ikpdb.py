172c3095f192089d4e9d1b678f341d71973a6443
Cyril MORISSE <cmorisse@boxes3.net>
2018-03-09 06:17:54 +0000

    Bump to Version 1.2.3

--- ikpdb.py
+++ ikpdb.py
@@ -32,7 +32,7 @@ import cgi
 
 # For now ikpdb is a singleton
 ikpdb = None 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 
 ##
 # Logging System
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 from setuptools import setup, find_packages, Extension
 
 name = 'ikpdb'
-version = '1.2.2'
+version = '1.2.3'
 
 
 long_description = (
